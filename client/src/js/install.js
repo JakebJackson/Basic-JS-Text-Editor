12711c12d4fa8189403a64693e08dc99ee7f369e
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2024-04-20 12:49:24 +1000

    First commit

---
+++ client/src/js/install.js
@@ -0,0 +1,32 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+
+    // Store triggered event(s)
+    window.deferredPrompt = event;
+
+    // Need to adjust the nav-btn class to display/hide the install button.
+
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
